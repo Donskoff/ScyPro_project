aec3f57544169dc93e380c04b4b6de9337095a2a
Donskov Andrey <bionet1971@gmail.com>
2025-05-11 11:24:16 +0500

    Successful completion of writing code

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,27 @@
+"""Программа обработки информации как о картах, так и о счетах."""
+
 from masks import get_mask_card_number, get_mask_account
 
+
 def mask_account_card(bank_details: str) -> str:
+    """Функция маскировки номера банковского счета или карты.
+
+    Функция принимает на вход номер счета или карты и возвращает его маску.
+    Номер счета замаскирован и отображается в формате Счёт **XXXX, а номер карты
+    замаскирован и отображается в формате Название карты XXXX XX** **** XXXX,
+    где X — это цифра номера.
+    """
     if "Счёт" in bank_details:
         bank_details_list = bank_details.split()
         bank_details_list[-1] = get_mask_account(bank_details_list[-1])
         new_bank_details = " ".join(bank_details_list)
         return new_bank_details
     else:
-        print("Это номер карты")
-    # return
+        bank_details_list = bank_details.split()
+        bank_details_list[-1] = get_mask_card_number(bank_details_list[-1])
+        new_bank_details = " ".join(bank_details_list)
+        return new_bank_details
+
 
 bank_details_data = input("Введите данные банковской карты или счёта ... ")
 
