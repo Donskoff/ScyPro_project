2fb1567a26a7dd2d7023259b2c8971d01ea4c2fd
Donskov Andrey <bionet1971@gmail.com>
2025-05-11 17:31:04 +0500

    Successful completion of the project

--- src/widget.py
+++ src/widget.py
@@ -29,7 +29,30 @@ def mask_account_card(bank_details: str) -> str:
         return "Введённые вами данные не корректны!"
 
 
+def get_date(current_date_data: str) -> str:
+    """Принимает на вход строку с датой в формате 2024-03-11T02:26:18.671407.
+
+    И возвращает строку с датой в формате ДД.ММ.ГГГГ (11.03.2024).
+    """
+    formatted_date = (
+        current_date_data[8]
+        + current_date_data[9]
+        + "."
+        + current_date_data[5]
+        + current_date_data[6]
+        + "."
+        + current_date_data[0]
+        + current_date_data[1]
+        + current_date_data[2]
+        + current_date_data[3]
+    )
+    return formatted_date
+
+
 bank_details_data = input("Введите данные банковской карты или счёта ... ")
 
 output_data = mask_account_card(bank_details_data)
 print(output_data)
+
+date_data = "2025-03-12T02:26:18.671407"
+print(get_date(date_data))
