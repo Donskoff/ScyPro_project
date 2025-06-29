4a5bcc59d8a78c3b794921cbc6af02c3f79b3954
Donskov <bionet1971@gmail.com>
2025-06-29 19:41:37 +0300

    editing before pull request

--- src/masks.py
+++ src/masks.py
@@ -13,7 +13,7 @@ def get_mask_card_number(card_number: str) -> str:
     if len(number_without_spaces) != 16 or not number_without_spaces.isdigit():
         return "Введён некорректный номер карты"
     # Форматируем по блокам по 4 цифры с пробелами
-    masked_card_number = " ".join(number_without_spaces[i: i + 4] for i in range(0, len(number_without_spaces), 4))
+    masked_card_number = " ".join(number_without_spaces[i : i + 4] for i in range(0, len(number_without_spaces), 4))
     # Преобразуем строку в список
     masked_card_number_list = list(masked_card_number)
     for i in range(len(masked_card_number_list)):
@@ -47,10 +47,3 @@ def get_mask_account(account_number: str) -> str:
         # Преобразуем список обратно в строку
     masked_account_number = "".join(count_without_spaces_list)
     return masked_account_number
-
-
-# num_card = input("Введите номер банковской карты ...")
-# print(get_mask_card_number(num_card))
-
-# num_card = input("Введите номер банковского счета ...")
-# print(get_mask_account(num_card))
