96cdc21e7f90793e01620e3d4a92561c5ff383e1
Donskov <bionet1971@gmail.com>
2025-07-08 21:02:50 +0300

    tests/test_decorators.py fixes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,7 +3,29 @@
 Проверяет функциональность декоратора.
 """
 
-from src.decorators import my_function  # , log
+import logging
+
+from src.decorators import my_function
+
+
+# Убедитесь, что логирование настроено для захвата выводов в тестах
+def setup_logging_for_tests():
+    """Настройка логгирования."""
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+
+def test_my_function(capsys):
+    """Тестирует успешное выполнение функции и логирование."""
+    setup_logging_for_tests()  # Настройка логирования для тестов
+
+    result = my_function(1, 7)  # Вызов функции
+    assert result == 8  # Проверка результата
+
+    # Перехватываем вывод
+    captured = capsys.readouterr()
+
+    # Проверяем, что в выводе есть ожидаемые строки
+    assert "Function my_function called whith (1, 7) and kwargs {}. Result: 8" in captured.out
 
 
 def test_log(capsys):
