2e18a6c57cafc6b1e2a476ec2dcda20b55098383
Donskov <bionet1971@gmail.com>
2025-07-11 17:04:09 +0300

    tests/test_external_api.py fixes

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -3,30 +3,39 @@
 Модуля extermal_api.py.
 """
 
-from unittest.mock import Mock, patch
+from unittest import mock
 
-from src.external_api import calculate_usd_to_rub, fetch_exchange_rates
+import pytest
 
-
-@patch("src.external_api.requests.get")  # Патчим requests.get
-def test_fetch_exchange_rates_success(mock_get):
-    """Тестируем с помощью @patch."""
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {"rates": {"USD": 1.2, "RUB": 75.0}}
-    mock_get.return_value = mock_response
-
-    # Вызываем тестируемую функцию
-    result = fetch_exchange_rates("http://data.fixer.io/api/latest", "dummy_access_key", "EUR", "USD,RUB")
-
-    # Проверяем результат
-    assert result == {"USD": 1.2, "RUB": 75.0}
-    mock_get.assert_called_once_with(
-        "http://data.fixer.io/api/latest",
-        params={"access_key": "dummy_access_key", "base": "EUR", "symbols": "USD,RUB"},
-    )
+from src.external_api import (convert_transaction_to_rub,  # Замените 'src.external_api' на правильный путь к вашему модулю
+                              fetch_exchange_rates)
 
 
-def test_calculate_usd_to_rub(calculate_usd_to_rub_fix):
-    """Тест с фикстурой."""
-    assert calculate_usd_to_rub(1, 2) == calculate_usd_to_rub_fix
+def test_fetch_exchange_rates_success(mocker):
+    """Тест."""
+    mock_response = {"rates": {"USD": 75.0, "RUB": 90.0}, "success": True}
+    mocker.patch("requests.get", return_value=mock.Mock(status_code=200, json=lambda: mock_response))
+
+    rates = fetch_exchange_rates("http://data.fixer.io/api/latest", "fake_api_key", "EUR", "USD,RUB")
+    assert rates == {"USD": 75.0, "RUB": 90.0}
+
+
+def test_fetch_exchange_rates_failure(mocker):
+    """Тест."""
+    mocker.patch("requests.get", return_value=mock.Mock(status_code=404))
+
+    with pytest.raises(ValueError, match="Ошибка: 404"):
+        fetch_exchange_rates("http://data.fixer.io/api/latest", "fake_api_key", "EUR", "USD,RUB")
+
+
+def test_convert_transaction_to_rub_usd(mocker):
+    """Тест."""
+    mock_rates = {"USD": 75.0, "RUB": 90.0}
+    mocker.patch("src.external_api.fetch_exchange_rates", return_value=mock_rates)  # Патчим fetch_exchange_rates
+
+    transaction = {"operationAmount": {"amount": "100", "currency": {"code": "USD", "name": "USD"}}}
+
+    result = convert_transaction_to_rub(transaction)
+    assert result["operationAmount"]["amount"] == 120.0  # 100 * (90 / 75)
+    assert result["operationAmount"]["currency"]["code"] == "RUB"
+    assert result["operationAmount"]["currency"]["name"] == "руб."
